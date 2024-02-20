13f67c8f5e1549f9e9114bc1e1e4609a0bd89fa9
H@rmit <Harmit@Harshits-MacBook-Air.local>
2024-02-20 16:36:08 +0530

    Update Portfolii

---
+++ js/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        name: 'HealthCare Products',
+        desc: 'The healthcare products website is fully equipped with essential e-commerce functionalities, such as adding items to the cart, accepting payments via G-Pay and Razorpay, facilitating user login, and categorizing products for easy browsing. It boasts a streamlined design and user-friendly interface.',
+        img: 'img/HealthcareProducts.jpeg',
+        code: 'https://github.com/Harmit1708/Healthcare-frontend',
+        demo: 'https://healthcare-products.netlify.app'
+    },
+    {
+        name: 'Beat With Music',
+        desc: 'beat with the song is an entertainment website users can listening favorite songs and take an enjoyment.',
+        img: 'img/beatwithmusic.png',
+        code: 'https://github.com/Harmit1708/Beatwithmusic-frontend',
+        demo: 'https://beatwithmusic.netlify.app/'
+    }
+]
